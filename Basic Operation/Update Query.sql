14db4f5934fd3d7bd6f63a88999ebbfc3f2f9f39
Tarun <tarundb2005@gmail.com>
2026-07-24 22:23:52 +0530

    Add SQL queries for employee data retrieval

---
+++ Basic Operation/Update Query.sql
@@ -0,0 +1,9 @@
+select employee_id, first_name , last_name from employees;
+
+select * from employees where employee_id = 3;
+
+select * from employees where last_name = "desu";
+
+select * from employees where hourly_pay >= 15;
+
+select * from employees where hire_date is null;
