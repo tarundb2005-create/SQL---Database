8459e1279f94fbcf2282f3367b4f46e1ca81f181
Tarun <tarundb2005@gmail.com>
2026-07-30 21:21:26 +0530

    Add SQL queries for employee data retrieval

---
+++ Basic Operation/% _ .sql
@@ -0,0 +1,11 @@
+select * from employees
+where first_name like "T%";
+
+select * from employees
+where last_name like "s%";
+
+select * from employees
+where first_name like "_ishva";
+
+select * from employees
+where hire_date like "____-01-__"
